80e25b94a131d3b2ee096147f02ae0133fd25bef
Olakunle <olatunjiolakunledave@gmail.com>
2023-09-30 21:47:54 -0400

    updated commit

--- src/routes/index.js
+++ src/routes/index.js
@@ -9,8 +9,7 @@ const router = express.Router();
 /**
  * Expose all of our API routes on /v1/* to include an API version.
  */
-router.use(`/v1`, require('./api'));
-router.use(`/v1`, authenticate(), require('./api'));
+router.use('/v1', authenticate(), require('./api'));
 
 /**
  * Define a simple health check route. If the server is running
